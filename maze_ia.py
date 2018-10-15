ea2f95c2c3fcd9dcd6759f59c17066154b0207dd
ldlong <longlamduc@gmail.com>
2018-10-15 14:25:24 +0700

    ds

--- maze_ia.py
+++ maze_ia.py
@@ -3,11 +3,11 @@ import sys
 def load_maze():
     line = sys.stdin.readline()
     maze = []
-    f = open("haha","a")
     if 'MAZE' in line:
         while len(line) > 0:
             line = sys.stdin.readline().strip()
-            maze.append(list(line))
+            maze.append(line)
+    f = open("maze_map", "w")
     f.write(str(maze))
     f.close()
     return maze
@@ -20,40 +20,56 @@ def bfs(maze, start, goal): #start is number and goal is character
     way = [] # give exactly the way from the start point to the goal point
     x = start[0]
     y = start[1]
-    check = [(x, y)]
+    check = [[x, y]]
     while maze[x][y] != goal:
-        for x2, y2 in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)):
-            if maze[x2][y2] != '#' and (x2, y2) not in check:
+        for x2, y2 in ((x - 1, y), (x, y + 1), (x + 1, y), (x,y-1)):
+            if maze[x2][y2] != '#' and [x2, y2] not in check:
                 before[(x2, y2)] = [x, y]
                 queue.append([x2, y2])
-                check.append((x2, y2))
+                check.append([x2, y2])
         x = queue[0][0]
         y = queue[0][1]
         queue.pop(0)
-    way.append([x, y])
     t = []
-    while (x, y) != start:
-        t = before[(x, y)]
-        way.append(t)
-        x = t[0]
-        y = t[1]
+    way.append([x,y])
+    while before[(x,y)] != start:
+        way.append(before[(x,y)])
+        x = before[(x,y)][0]
+        y = before[(x,y)][1]
+    way.append(before[(x,y)])
     return way #guide to go from start to goal step by step
 
 
 def move(way):
-    x = way[len(way) - 1][0]
-    y = way[len(way) - 1][1]
-    for i in reversed(range(len(way)) - 1):
-        x = way[i + 1][0]
-        y = way[i + 1][1]
-        if way[i][0] - x == 1:
-            sys.stdout.write("MOVE DOWN\n\n")
-        elif way[i][0] == 1:
-            sys.stdout.write("MOVE UP\n\n")
-        elif way[i][i] - y == 1:
-            sys.stdout.write("MOVE RIGHT\n\n")
-        else:
-            sys.stdout.write("MOVE LEFT\n\nS")
+    f = open("test", "w")
+    i = len(way) - 2
+    f.write(str(i))
+    f.write(str(way) + '\n')
+    # while i >= 0:
+    x = way[i + 1][0]
+    y = way[i + 1][1]
+    if way[i][0] - x == 1:
+        sys.stdout.write("MOVE DOWN\n\n")
+        f.write("MOVE DOWN\n\n")
+    elif x - way[i][0] == 1:
+        sys.stdout.write("MOVE UP\n\n")
+        f.write("MOVE UP\n\n")
+    elif way[i][1] - y == 1:
+        sys.stdout.write("MOVE RIGHT\n\n")
+        f.write("MOVE RIGHT\n\n")
+    elif y - way[i][1] == 1:
+        sys.stdout.write("MOVE LEFT\n\n")
+        f.write("MOVE LEFT\n\n")
+        # i -= 1
+        # for _ in range(len(way) - 1):
+        #     line = sys.stdin.readline()
+        # line = sys.stdin.readline()
+        # line = sys.stdin.readline()
+        # f.write(line)
+        # while len(line) > 0:
+        #     line = sys.stdin.readline()
+        #     f.write(line)
+    f.close()
 
 sys.stdin.readline()
 sys.stdout.write("I AM IA\n\n")
@@ -63,9 +79,6 @@ sys.stdout.write("OK\n\n")
 sys.stdin.readline()
 maze = []
 maze = load_maze()
-f = open('maze_map', 'a')
-f.write(str(maze) + '\n')
-f.close()
 start = []
 for x in range(len(maze)):
     for y in range(len(maze[x])):
@@ -78,5 +91,6 @@ for x in range(len(maze)):
 #     # if len(way) > 0 and len(way) < 20:
 #     #     move(way)
 #     # else:
-way = bfs(maze, start, 'o')
-move(way)
+for _ in range(1000):
+    way = bfs(maze, start, 'o')
+    move (way)
